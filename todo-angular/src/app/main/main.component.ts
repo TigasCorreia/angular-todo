6628df2789e0d3f32b145a516736090bde431ca4
TigasCorreia <tiagoseabracorreia@outlook.pt>
2024-01-19 16:00:38 +0000

    Figured out how to use imports in angular

--- todo-angular/src/app/main/main.component.ts
+++ todo-angular/src/app/main/main.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ContainerComponent } from '../container/container.component';
+
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  imports: [ContainerComponent], 
+  template: '<app-container></app-container>',
+  styleUrl: './main.component.css',
 })
+
 export class MainComponent {
 
 }
--- todo-angular/src/main.server.ts
+++ todo-angular/src/main.server.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
+import { MainComponent } from './app/main/main.component';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = () => bootstrapApplication(MainComponent, config);
 
 export default bootstrap;
